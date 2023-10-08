c07bc99bba7521bc57bb6324fa2fb9b5ec67e78b
muniznicole <nicoleamuniz@gmail.com>
2023-10-07 23:50:04 -0300

    Create imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,20 +4,37 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
-<<<<<<< HEAD
-insert into estado (nomeEstadoCompleto,abreviado) values ('Tocantins','TO');
-insert into cidade (nomeCidade,id_estado) values ('Palmas',1);
-insert into cidade (nomeCidade,id_estado) values ('Paraiso',1);
-insert into endereco (quadra,alameda,complemento,numero,cep,id_cidade) values('vale do sol','rua bois bravos','casa de esquina muro azul','05','12345',1);
-=======
 
-insert into item (nomeItem, descricaoItem, precoItem, tamanho_tipo, imagemItem) values('Caderno da Barbie','Caderno Inteligente Grande', 40.00, 'GRANDE', 'Add Imagem');
-insert into item (nomeItem, descricaoItem, precoItem, tamanho_tipo, imagemItem) values('Caderno da Barbie','Caderno Inteligente Medio', 30.00, 'MEDIO', 'Add Imagem');
-insert into item (nomeItem, descricaoItem, precoItem, tamanho_tipo, imagemItem) values('Caderno da Barbie','Caderno Inteligente Pequeno', 20.00, 'PEQUENO', 'Add Imagem');
->>>>>>> d42c43cdbeb5e67005a5e5fb361e8f40dbd6df94
+-- CRUDS ANDRÉ
+insert into estado (nomeEstadoCompleto,abreviado) values('Tocantins','TO');
+insert into estado (nomeEstadoCompleto,abreviado) values('Goiás','GO');
+insert into estado (nomeEstadoCompleto,abreviado) values('Rio de Janeiro','RJ');
+
+insert into cidade (nomeCidade,id_estado) values('Palmas',1);
+insert into cidade (nomeCidade,id_estado) values('Paraiso',1);
+insert into cidade (nomeCidade,id_estado) values('Colinas',1);
+
+insert into endereco (quadra,alameda,complemento,numero,cep,id_cidade) values('vale do sol','rua bois bravos','casa de esquina muro azul','05','12345',1);
+insert into endereco (quadra,alameda,complemento,numero,cep,id_cidade) values('Centro','rua bois bravos','casa de esquina muro azul','05','12345',1);
+insert into endereco (quadra,alameda,complemento,numero,cep,id_cidade) values('Borte','rua bois bravos','casa de esquina muro azul','05','12345',1);
 
 insert into usuario (nomeCompleto,cpf,login,telefone,email,senha,perfil) values('André Barreira de Catro','1234556','Quico','63985139583','andreghoul18@gmail.com','123456','ADMIN');
 insert into usuario (nomeCompleto,cpf,login,telefone,email,senha,perfil) values('Neuma Barreira de Catro','1234556','Quico','63985139583','andreghoul18@gmail.com','123456','CLIENTE');
 insert into usuario (nomeCompleto,cpf,login,telefone,email,senha,perfil) values('Ivan Barreira de Catro','1234556','Quico','63985139583','andreghoul18@gmail.com','123456','ADMIN');
 insert into usuario (nomeCompleto,cpf,login,telefone,email,senha,perfil) values('Andreia Barreira de Catro','1234556','Quico','63985139583','andreghoul18@gmail.com','123456','CLIENTE');
+
+-- CRUDS NICOLE
+insert into item (nomeItem, descricaoItem, precoItem, tamanho_tipo, imagemItem) values('Caderno da Barbie','Caderno Inteligente Grande', 40.00, 'GRANDE', 'Add Imagem');
+insert into item (nomeItem, descricaoItem, precoItem, tamanho_tipo, imagemItem) values('Caderno da Barbie','Caderno Inteligente Medio', 30.00, 'MEDIO', 'Add Imagem');
+insert into item (nomeItem, descricaoItem, precoItem, tamanho_tipo, imagemItem) values('Caderno da Barbie','Caderno Inteligente Pequeno', 20.00, 'PEQUENO', 'Add Imagem');
+
+insert into estoque (id_item, descricaoEstoque, quantidade) values(1, 'Texto1', 10);
+insert into estoque (id_item, descricaoEstoque, quantidade) values(2, 'Texto2', 20);
+insert into estoque (id_item, descricaoEstoque, quantidade) values(3, 'Texto3', 30);
+
+insert into carrinho (id_item, id_usuario, precoTotalCarrinho) values(1, 1, 100.00);
+insert into carrinho (id_item, id_usuario, precototalcarrinho) values(2, 2, 200.00);
+insert into carrinho (id_item, id_usuario, precototalcarrinho) values(3, 3, 300.00);
+
+
 
