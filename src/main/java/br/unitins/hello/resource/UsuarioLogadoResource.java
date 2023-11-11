308382c2800fda839f5d11480172941d953b1073
muniznicole <nicoleamuniz@gmail.com>
2023-11-11 00:22:46 -0300

    Criação do Usuário Logado

---
+++ src/main/java/br/unitins/hello/resource/UsuarioLogadoResource.java
@@ -0,0 +1,37 @@
+package br.unitins.hello.resource;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import br.unitins.hello.service.UserService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/usuariologado")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UsuarioLogadoResource {
+
+    @Inject
+    JsonWebToken jwt;
+
+    @Inject
+    UserService usuarioService;
+
+    @GET
+    @RolesAllowed({ "User", "Admin" })
+    public Response getUsuario() {
+
+        // obtendo o login pelo token jwt
+        String login = jwt.getSubject();
+
+        return Response.ok(usuarioService.findByLogin(login)).build();
+
+    }
+
+}
