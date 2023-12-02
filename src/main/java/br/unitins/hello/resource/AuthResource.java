d464b449cfe45c2aa919060b85d1b69b7993aca6
muniznicole <nicoleamuniz@gmail.com>
2023-12-02 01:04:22 -0300

    add logs

--- src/main/java/br/unitins/hello/resource/AuthResource.java
+++ src/main/java/br/unitins/hello/resource/AuthResource.java
@@ -1,11 +1,14 @@
 package br.unitins.hello.resource;
 
+import org.jboss.logging.Logger;
+
 import br.unitins.hello.dto.LoginDTO;
 import br.unitins.hello.dto.UserResponseDTO;
 import br.unitins.hello.model.Usuario;
 import br.unitins.hello.service.HashService;
 import br.unitins.hello.service.JwtService;
 import br.unitins.hello.service.UserService;
+
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
@@ -22,18 +25,36 @@ public class AuthResource {
 
     @Inject
     UserService service;
-
+    
     @Inject
     HashService hashService;
-
+    
     @Inject
     JwtService jwtService;
 
+    private static final Logger LOG = Logger.getLogger(AuthResource.class);
+
     @POST
     public Response login (@Valid LoginDTO dto ){
+
+        LOG.infof("Iniciando a autenticacao do %s", dto.login());
+
         String hashSenha = hashService.getHashSenha(dto.senha());
+
+        LOG.info("Hash da senha gerado.");
+
+        LOG.debug(hashSenha);
+
         UserResponseDTO result = service.findByLogineSenha(dto.login(), hashSenha);
+
+        if (result != null)
+            LOG.info("Login e senha corretos.");
+        else
+            LOG.info("Login e senha incorretos.");
+
         String token = jwtService.generateJwt(result);
+
+        LOG.info("Finalizando o processo de login.");
 
         return Response.ok().header("Authorization", token).build();
 
